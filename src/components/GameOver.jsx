34670028edc8cc101289ff065074ccaf3ef14e07
basam <basam.najwa11@gmail.com>
2025-07-08 08:10:56 +0400

    added accessibility features to the div

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,8 +1,14 @@
+import { useRef, useEffect } from "react";
+
 import RegularButton from "./RegularButton";
 
 const GameOver = ({ handleClick }) => {
+  const focusRef = useRef(null);
+  useEffect(() => {
+    focusRef.current.focus();
+  }, []);
   return (
-    <div className="wrapper wrapper--accent">
+    <div className="wrapper wrapper--accent" ref={focusRef} tabIndex={-1}>
       <p className="p--large">You have matched all memory cards!</p>
       <RegularButton handleClick={handleClick}>Play again</RegularButton>
     </div>
